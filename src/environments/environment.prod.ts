421c4250875a66cf8381fbba988375dae9e63a6d
Manzoor Alam <manzoor.alam@thinktac.com>
2023-03-15 11:04:57 +0530

    Hosted

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyAY19yM5aWif5eOLr83XOZbZ7dLEsZ4CyI",
+        authDomain: "thinktac-games.firebaseapp.com",
+        projectId: "thinktac-games",
+        storageBucket: "thinktac-games.appspot.com",
+        messagingSenderId: "673115979014",
+        appId: "1:673115979014:web:c6e7b836c8d26e41403287",
+        measurementId: "G-7HDNM5JSB8"
+    }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAY19yM5aWif5eOLr83XOZbZ7dLEsZ4CyI",
+        authDomain: "thinktac-games.firebaseapp.com",
+        projectId: "thinktac-games",
+        storageBucket: "thinktac-games.appspot.com",
+        messagingSenderId: "673115979014",
+        appId: "1:673115979014:web:c6e7b836c8d26e41403287",
+        measurementId: "G-7HDNM5JSB8"
+    }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
